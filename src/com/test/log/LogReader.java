3857811777de986700e19aea94417855d4fd4131
sunilpandey1989 <sunil19vip@yahoo.co.in>
2020-02-23 09:25:53 +0530

    Optimized code to get the expected result

--- src/com/test/log/LogReader.java
+++ src/com/test/log/LogReader.java
@@ -2,98 +2,103 @@ package com.test.log;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.FileNotFoundException;
 import java.io.InputStreamReader;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.InputMismatchException;
+import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import com.test.model.ServiceData;
 
 public class LogReader {
-	static HashMap<String,ServiceData> allEntries = new HashMap<String,ServiceData>();
-	static HashMap<String,Long> maxExecutionEntries = new HashMap<String,Long>();
-	
-	public static void main(String[] args) throws IOException {
-			initLoader("test.log");
-			
-			System.out.println("Enter service Name");
-			Scanner scanner = new Scanner(System.in);
-			String serviceName = scanner.next();
-			System.out.println("Total Number of request to service "+ serviceName + " =" + getExecutionNumber(serviceName));
-			
-			scanner.close();
-			System.out.println("Max time taken for the service :" + maxExecutionEntries.get(serviceName));
-			
-		
+	private final static Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+	static HashMap<String, ServiceData> allEntries = new HashMap<String, ServiceData>();
+	static HashMap<String, Long> maxExecutionEntries = new HashMap<String, Long>();
+	static String fileName = "test.log";
+
+	public static void main(String[] args) {
+		// Parsing the log file
+		initLoader();
+
+		Map<String, Integer> serviceExecutionMap = getSvcExecutionNumber();
+		for (String serviceName : serviceExecutionMap.keySet()) {
+			System.out.println(
+					"Amount of request to service \"" + serviceName + "\" is " + serviceExecutionMap.get(serviceName)
+							+ " and max time for request execution is " + maxExecutionEntries.get(serviceName)/1000 +" seconds.");
+		}
 	}
-	
-	public static int getExecutionNumber(String serviceName) {
-		int count=0;
-		for(Entry<String,ServiceData> entry: allEntries.entrySet()) {
-			if(entry.getValue().serviceName.equals(serviceName)) {
-				count++;
+
+	public static Map<String, Integer> getSvcExecutionNumber() {
+		Map<String, Integer> serviceMap = new HashMap<String, Integer>();
+		for (Entry<String, ServiceData> entry : allEntries.entrySet()) {
+			if (serviceMap.containsKey(entry.getValue().serviceName)) {
+				serviceMap.put(entry.getValue().serviceName, serviceMap.get(entry.getValue().serviceName) + 1);
+			} else {
+				serviceMap.put(entry.getValue().serviceName, 1);
 			}
 		}
-		if(count == 0)
-			throw new InputMismatchException("Provide valid serviceName");
-		return count;
+		return serviceMap;
 	}
 
+	//Extract Service Name
 	public static String getServiceName(String input) {
 		int len = input.length();
-		return input.substring(1,len-1).split(":")[0];
+		return input.substring(1, len - 1).split(":")[0];
 	}
 
+	//Extract Service ID
 	public static String getServiceId(String input) {
 		int len = input.length();
-		return input.substring(1,len-1).split(":")[1];
+		return input.substring(1, len - 1).split(":")[1];
 	}
-	
-	public static HashMap<String,ServiceData> initLoader(String fileName) throws IOException{
-		FileInputStream fs = new FileInputStream(System.getProperty("user.dir")+"\\src\\com\\test\\log\\"+fileName);
-		BufferedReader reader = new BufferedReader(new InputStreamReader(fs));
-		
 
-		while(reader.readLine() != null) {
-			try {
+	public static HashMap<String, ServiceData> initLoader() {
 
-				String readLine = reader.readLine();
+		FileInputStream fs;
+		try {
+			fs = new FileInputStream(System.getProperty("user.dir") + "\\src\\com\\test\\resource\\" + fileName);
+
+			BufferedReader reader = new BufferedReader(new InputStreamReader(fs));
+			String readLine = null;
+			while ((readLine = reader.readLine()) != null) {
+
 				String[] items = readLine.split(" ");
-				//2015-10-26T16:09:47,958
+				// 2015-10-26T16:09:47,958
 				SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss,SSS");
 				Date date = df.parse(items[0]);
 				long epoch = date.getTime();
 
-				if(items[3].equals("entry")) {
+				if (items[3].equals("entry")) {
 					ServiceData sd = new ServiceData(getServiceName(items[5]), Long.parseLong(getServiceId(items[5])));
 					sd.setEntryTime(epoch);
 					allEntries.put(getServiceId(items[5]), sd);
 				}
-
-				else if(items[3].equals("exit")) {
+				else if (items[3].equals("exit")) {
 					ServiceData sd = allEntries.get(getServiceId(items[5]));
-					sd.setExitTime(epoch);
-					allEntries.put(getServiceId(items[5]), sd);
-					if(maxExecutionEntries.containsKey(sd.serviceName)) {
-						if(maxExecutionEntries.get(sd.serviceName) < (sd.exitTime -sd.entryTime)) {
-							maxExecutionEntries.put(sd.serviceName, sd.exitTime -sd.entryTime);
-						}
-					}  
-					else
-						maxExecutionEntries.put(sd.serviceName, sd.exitTime - sd.entryTime);	
+					if (sd != null) {
+						sd.setExitTime(epoch);
+						allEntries.put(getServiceId(items[5]), sd);
+						if (maxExecutionEntries.containsKey(sd.serviceName)) {
+							if (maxExecutionEntries.get(sd.serviceName) < (sd.exitTime - sd.entryTime)) {
+								maxExecutionEntries.put(sd.serviceName, sd.exitTime - sd.entryTime);
+							}
+						} else
+							maxExecutionEntries.put(sd.serviceName, sd.exitTime - sd.entryTime);
+					}
 				}
-
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
 			}
-		}
+		
 
 		reader.close();
+		} catch (FileNotFoundException fe) {
+			LOGGER.log(Level.SEVERE, "Specified file path is not found, Details: " +fe.getMessage());
+		} catch (Exception e) {
+			LOGGER.log(Level.SEVERE, "Execution failed, Details:  "+ e.getMessage());
+		}
 		return allEntries;
 	}
 
--- src/com/test/model/ServiceData.java
+++ src/com/test/model/ServiceData.java
@@ -1,40 +1,44 @@
 package com.test.model;
 
 public class ServiceData {
+	
+	public String serviceName;
+	public long serviceId;
+	public long entryTime;
+	public long exitTime;
+	
 	@Override
 	public String toString() {
 		return "ServiceData [serviceName=" + serviceName + ", serviceId=" + serviceId + ", execution time ="
-				+ (exitTime-entryTime) + "]";
+				+ (exitTime - entryTime) + "]";
 	}
 
 	public ServiceData(String serviceName, long serviceId) {
 		this.serviceName = serviceName;
 		this.serviceId = serviceId;
-		
 	}
-	public String serviceName;
+
 	public String getServiceName() {
 		return serviceName;
 	}
 
-	public void setServiceId(Long serviceId) {
+	public void setServiceId(long serviceId) {
 		this.serviceId = serviceId;
 	}
+
 	public long getEntryTime() {
 		return entryTime;
 	}
+
 	public void setEntryTime(long entryTime) {
 		this.entryTime = entryTime;
 	}
+
 	public long getExitTime() {
 		return exitTime;
 	}
+
 	public void setExitTime(long exitTime) {
 		this.exitTime = exitTime;
 	}
-	public Long serviceId;
-	public long entryTime;
-	public long exitTime;
-	
-	
 }
